0fa95ee7b8eef82cd8458b01ad1f5454da2e891a
chaimae <chaimaer759@gmail.com>
2024-11-08 16:24:36 +0100

    first commit

---
+++ src/test/java/com/jenkins/tp3springboot/Tp3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.tp3springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
